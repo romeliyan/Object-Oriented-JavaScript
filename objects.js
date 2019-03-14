88d76a01ed1017e1538c53977105f081bf1111f7
romeliyan <rkbliyanage@gmail.com>
2019-03-14 14:10:16 +0530

    without using objects

--- objects.js
+++ objects.js
@@ -1,3 +1,25 @@
-var names = ['roy','nimal','mario'];
+//without using objects
 
-console.log(names.length);
+var userOneEmail = 'roy@gmail.com';
+var userOneNAme = 'roy';
+var userOneFriends = ['nimal'];
+
+var userTwoEmail = 'niaml@gmail.com';
+var userTwoNAme = 'niaml';
+var userTwoFriends = ['roy', 'mario'];
+
+function login(email){
+    console.log(email, 'is now logged in');
+}
+
+function logout(email){
+    console.log(email, 'is now logged out');
+}
+
+function logFriends(friends){
+   friends.forEach(friend => {
+       console.log(friends);
+   });
+}
+
+login(userOneEmail);
