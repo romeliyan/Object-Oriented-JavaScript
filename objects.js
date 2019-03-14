dda45b9c3ee72469cab963d0c2c7b6e16369f15d
romeliyan <rkbliyanage@gmail.com>
2019-03-14 14:41:21 +0530

    ways of access and update properties and methods

---
+++ objects.js
@@ -0,0 +1,18 @@
+//access properties
+
+userOne.email;
+//or
+userOne['email'];
+
+
+//update properties
+userOne.email = 'roy@yahoo.com';
+//or
+userOne['email'] = 'roy@yahoo.com';
+
+
+//create new / add on properties
+userOne.age = 25;
+
+// this is applicable for methods as well
+userOne.logInfo = function () {  } ;
